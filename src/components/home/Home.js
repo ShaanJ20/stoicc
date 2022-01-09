d0c6a0a996b3e70378fa75129f4d26316e1c0570
ShaanJ20 <shaan.johari18@gmail.com>
2022-01-09 16:38:02 +0530

    skeleton for the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './home.css'
 
 export default function Home() {
     return (
         <>
-            
+
+            <div className="container">
+
+                <h1 className='header'></h1>
+                <h1 className='subheader'></h1>
+                <div className="button-container">
+                    <button className='continue-button'></button>
+                </div>
+            </div>
+
         </>
     )
 }
