050e4fea59a048590dff68ed6f851cd8bbc526c0
ShaanJ20 <shaan.johari18@gmail.com>
2022-01-09 17:09:57 +0530

    Styling - animations

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,11 +7,14 @@ export default function Home() {
 
             <div className="container">
 
-                <h1 className='header'></h1>
-                <h1 className='subheader'></h1>
+                <h1 className='header'>stoic</h1>
+                <p className='subheader-1'>develop perspectives</p>
+                <p className='subheader-2'>be happy</p>
+                <p className='subheader-3'>be peacefull</p>
                 <div className="button-container">
-                    <button className='continue-button'></button>
+                    <button className='continue-button'>start</button>
                 </div>
+
             </div>
 
         </>
